323048a348bbed657a636300056b3939e069fa0f
nexossama <ossamaoutmani@gmail.com>
2024-05-20 04:36:27 +0100

    modify .helpers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .routes import base , generate_story
-from helpers.config import get_settings
+from .helpers.config import get_settings
 
 app = FastAPI()
 app.include_router(base.base_router)
